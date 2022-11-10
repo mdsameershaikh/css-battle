2a2a8dbf3c2ceb52ca56d95bf55b31b55ae161f6
sameer shaikh <sameershaikh@gofynd.com>
2022-11-10 19:15:05 +0530

    created basic structure

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="main-container">
+      <div class="side-panel"></div>
+      <div class="cards-container">
+
+    </div>
+    </div>
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  data(){
+    return {
+      height:''
+    }
+  },
+  mounted(){
+    this.height= screen.height +"px" 
   }
-}
+};
 </script>
 
 <style>
+body {
+  background-color: #2c3e50;
+}
+.main-container{
+  display: grid;
+  grid-template-columns: 1fr 6fr;
+  grid-gap: 50px;
+}
+.side-panel{
+  background: #2c3e50;
+  border: 8px solid white;
+  height: calc(v-bind('height') - 7rem);
+}
+.cards-container{
+   border: 8px solid white;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: auto;
+  width: auto;
 }
 </style>

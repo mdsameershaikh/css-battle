53b50fcb8aa4e805e0689595ccd7b7df459e2d25
Sameer Shaikh <shaikhs975@gmail.com>
2023-12-15 12:11:56 +0530

    installed vuex as well as refactored main js code lil bit

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
-
-new Vue({
+var app = new Vue({
   render: h => h(App),
-}).$mount('#app')
+})
+app.$mount('#app')
